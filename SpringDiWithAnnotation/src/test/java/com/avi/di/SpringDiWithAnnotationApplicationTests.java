0853b281216a24375f19f7cd89ca373449f659e0
Avinash <Avinash@Avinash-NXM8ESI00234.bbrouter>
2023-05-22 23:23:39 +0530

    spring DI

---
+++ SpringDiWithAnnotation/src/test/java/com/avi/di/SpringDiWithAnnotationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avi.di;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDiWithAnnotationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
